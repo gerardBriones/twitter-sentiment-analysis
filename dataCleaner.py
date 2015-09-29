d955e8a772a0dfd6b6be06ee44cee31d820c702a
Gerard Briones <brionesgc@vcu.edu>
2015-09-29 18:04:55 -0400

    added dataCleaner that kasun created implemented dataCleaner in preprocessor.py reworked preprocessor.py to map tweet_id to bag of words (for that specific tweet)

---
+++ dataCleaner.py
@@ -0,0 +1,36 @@
+# InformationExtractor.py
+# Testing, removing special characters and lower case
+
+import re
+
+supporting_data_path = "supporting_data/"
+s = "Gas by my house hit $3.39!!!! I'm going to Chapel Hill on Sat. :)"
+
+# perform initial preprocessing
+# returns all lower case string without special characters, numbers etc. 
+def removeSpecialCharacters(str):
+	new_str = re. sub('[^A-Za-z ]+', '', str)
+	new_str = new_str.lower()
+	return new_str 
+
+# Removes stop words from the string
+# Before calling this method, initial_preprocessing has to be called
+# pass in the initial preprocessed string as the argument
+# returns the tokens sans stop words from the tweet
+def removeStopWords(str):
+	# extracting the set of words in the string 	
+	word_list = str.split();
+
+	stopWord_Filename = "stopWord_list.txt"
+
+	# stop words should be space delimited
+	sw_fp = open(supporting_data_path + stopWord_Filename)
+	for row in sw_fp:
+		row = re. sub('[^A-Za-z ]+', '', row)
+		sw_list = row.split()
+	word_list = str.split();
+	key_words = []
+	for word in word_list:
+		if word not in sw_list:
+			key_words.append(word)
+	return key_words
--- preprocessor.py
+++ preprocessor.py
@@ -1,9 +1,10 @@
 # preprocessor.py
 # Authors: Gerard Briones, Kasun Amarasinghe
-# Package Dependencies: None
+# Package Dependencies: none
 
 import csv
 import os
+import dataCleaner
 
 # training data directory
 training_data_path = 'train_data/'
@@ -17,8 +18,29 @@ tweet_unigrams = {}
 # dict for unigrams
 unigrams = {}
 
+# 2D list mapping tweet_id to list of words
+# 
+# tweet_ids 	|	word1	|	word2	|	... 	|	wordN
+# tweet_1		|	1		|	0		|			|	2
+# tweet_2		|	0		|	3		|			|	1
+# tweet_3		|	2		|	4		|			|	1
+list_of_tweets = []
+
 # for each file (tsv) in the training data directory...
 for filename in os.listdir(training_data_path):
+
+	# FIRST ROUND OF PROCESSING
+	# populate tweet_ids{} and unigrams{} with key value pairs
+	# tweet_ids{}:
+	# 	key = tweet_id
+	# 	value = index
+	#
+	# unigrams{}:
+	# 	key = word
+	# 	value = index
+	#
+	# these are designed to map tweet_ids and words to an
+	# easily accessible index
 
 	# open the input file
 	with open(training_data_path + filename, 'rb') as tsv_in:
@@ -26,52 +48,101 @@ for filename in os.listdir(training_data_path):
 		# create csv reader for the input file
 		tsv_in = csv.reader(tsv_in, delimiter='\t')
 
+		# counter of tweets and words in order to assign and track indices
+		tweetCounter = 0
+		wordCounter = 0
+
 		# ROW PATTERN - One tweet per line
 		#	0	1	  2				3			4			 5
 		# id1 | id2 | start_token | end_token | pred_class | text
 		
 		# for each tweet in file...
 		for row in tsv_in:
-			tweet_id = str(row[0])
-			tokens = row[5].lower().split()
-			temp_unigram_dict = {}
 
-			if not tweet_id in tweet_ids:
+			# if tweet is not available...
+			if row[5] == 'Not Available':
+				pass # do nothing!
 
-				# add tweet_id to tweet_ids{}
-				tweet_ids[tweet_id] = 1
-
-				# add tweet_id to tweet_unigrams{}
-				tweet_unigrams[tweet_id] = {}
-
-				# for each unigram in tweet...
-				for token in tokens:
-
-					# add unigram to unigrams{}
-					if not token in unigrams:
-						unigrams[token] = 1
-
-					# increment unigram count in unigrams{}	
-					else:
-						unigrams[token] += 1
-
-					# add unigram to tweet_unigrams[tweet_id]{}
-					if not token in tweet_unigrams[tweet_id]:
-						tweet_unigrams[tweet_id][token] = 1
-
-					# increment unigram count in tweet_unigrams[tweet_id]{}
-					else:
-						tweet_unigrams[tweet_id][token] += 1
 			else:
-				# increment tweet_id count in tweet_ids{}
-				tweet_ids[tweet_id] += 1
+				tweet_id = str(row[0])
 
+				# lowercasify, remove special characters, and remove stop words
+				# dataCleaner -- removeSpecialCharacters()
+				#		removes special characters
+				# dataCleaner -- removeStopWords() 
+				#		removes stop words
+				tokens = dataCleaner.removeStopWords(dataCleaner.removeSpecialCharacters(row[5].lower()))
 
+				# first occurance of tweet id, to prevent tracking duplicates
+				if not tweet_id in tweet_ids:
 
-# printing unigrams to terminal
-# for unigram in unigrams:
-# 	print unigram, ' : ', unigrams[unigram]
+					# add tweet_id to tweet_ids{} with a value of its associated index
+					tweet_ids[tweet_id] = tweetCounter
+					tweetCounter += 1
 
-for tid in tweet_unigrams:
-	for unigram in tweet_unigrams[tid]:
-		print tid, ' : ', unigram, ' : ', tweet_unigrams[tid][unigram]
+					# for each word in tweet...
+					for token in tokens:
+
+						# first occurance of word, to prevent tracking duplicates
+						if not token in unigrams:
+
+							# add word to unigrams{} with a value of its associated index
+							unigrams[token] = wordCounter
+							wordCounter += 1
+
+	# SECOND ROUND OF PROCESSING
+	# populate list_of_tweets[] with tweet_ids and messages
+	# [] first index = tweet_ids[tweet_id]
+	# value = index
+	#
+	# this is built this way in order to easily track unique tweet_ids
+	# and words in order to properly build the 2D array
+
+	# reopen the input file
+	with open(training_data_path + filename, 'rb') as tsv_in2:
+
+		# create csv reader for the input file
+		tsv_in2 = csv.reader(tsv_in2, delimiter='\t')
+
+		# initialize first dimension of list_of_tweets for tweet_ids
+		list_of_tweets = [0] * (len(tweet_ids))
+
+		# initialize second dimension of list_of_tweets for bag of words
+		for i in range(0, len(tweet_ids)):
+			list_of_tweets[i] = [0] * (len(unigrams))
+
+		# temporary dict to keep track of tweet_ids
+		temp_tweet_ids = {}
+
+		# ROW PATTERN - One tweet per line
+		#	0	1	  2				3			4			 5
+		# id1 | id2 | start_token | end_token | pred_class | text
+		
+		# for each tweet in file...
+		for row in tsv_in2:
+			
+			# if tweet is not available...
+			if row[5] == 'Not Available':
+				pass # do nothing!
+
+			else:
+				tweet_id = str(row[0])
+
+				# lowercasify, remove special characters, and remove stop words
+				# dataCleaner -- removeSpecialCharacters()
+				#		removes special characters
+				# dataCleaner -- removeStopWords() 
+				#		removes stop words
+				tokens = dataCleaner.removeStopWords(dataCleaner.removeSpecialCharacters(row[5].lower()))
+
+				# only tracking unique tweet_ids
+				if not tweet_id in temp_tweet_ids:
+
+					# mark tweet as seen
+					temp_tweet_ids[tweet_id] = 1
+
+					# build bag of words for each unique tweet
+					for token in tokens:
+						list_of_tweets[tweet_ids[tweet_id]][unigrams[token]] += 1
+
+print unigrams
