d44b6efed4258283aa6fe758abda87445ecbae3a
Gerard Briones <brionesgc@vcu.edu>
2015-09-30 23:45:17 -0400

    runit.sh should now properly execute the commands output folder has been added

--- runit.sh
+++ runit.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+#create feature vector for baseline
 python featureVectorCreator_BL.py
+
+#run weka on baseline feature vector
+java -Xmx2G -cp /usr/share/java/weka.jar weka.classifiers.bayes.NaiveBayes -o -t arff_data/arff_train_bl.arff -T arff_data/arff_test_bl.arff -k > output/weka_bl.txt
+
+#create feature vector for pm (presented method)
+python featureVectorCreator_PM.py
+
+#run weka on pm feature vector
+java -Xmx2G -cp /usr/share/java/weka.jar weka.classifiers.bayes.NaiveBayes -o -t arff_data/arff_train_pm.arff -T arff_data/arff_test_pm.arff -k > output/weka_pm.txt
